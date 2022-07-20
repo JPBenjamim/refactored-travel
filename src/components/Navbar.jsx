33860afc6d9931de15683d0cafeb93093ad85322
Jpbenjamim <dev.bejamim@gmail.com>
2022-07-20 16:53:44 -0300

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import logo from "/src/assets/logojj.png";
-import "/src/components/Navbar.css";
-
+import React from 'react'
+import logo from '/src/assets/logojj.png'
+import styles from './Navbar.module.css'
 
 function Navbar() {
-  
-
   return (
-    <div className="container bg-light   navBar" >
-      <div> 
-        <img src={logo} alt="" className="logo"/> <button className="btn btn-primary"> <a href="#" className="contact"> Contato</a>  </button></div>
-   
+    <div className={`container   ${styles.containerNavbar}`}>
+      
+        <img src={logo} alt="" className={styles.logo} />{' '}
+        <button className= "btn btn-primary">
+          
+          <a className={styles.contact} href="#">            
+            Contato
+          </a>
+        </button>
+     
     </div>
   )
 }
