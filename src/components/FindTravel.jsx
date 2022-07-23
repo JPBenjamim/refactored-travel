462158bc180cf851f0913b35ea8a39ecba022351
Jeffesson Gomes <jeffessong4@gmail.com>
2022-07-23 14:37:51 -0300

    feat(FindTravel): creating component FindTravel

---
+++ src/components/FindTravel.jsx
@@ -0,0 +1,13 @@
+import styles from './FindTravel.module.css';
+
+export function FindTravel(){
+    return(
+        <section className='container'>
+            <div className='row'>
+                <div className='col-12'>
+                    a
+                </div>
+            </div>
+        </section>
+    )
+}
